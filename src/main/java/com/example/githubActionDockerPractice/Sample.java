455a0d59a31477654c09c74e227be697e8519da4
sunghun <leedool3003@gmail.com>
2023-05-30 21:49:58 +0900

    change title2

--- src/main/java/com/example/githubActionDockerPractice/Sample.java
+++ src/main/java/com/example/githubActionDockerPractice/Sample.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Sample {
     @RequestMapping("/hello")
     public String greeting() {
-        return "test";
+        return "change";
     }
 }
